f38498e3273491837b66932e82ac62d1c51fd712
Arthur Vargas Pinheiro <269752999+arthurpiro@users.noreply.github.com>
2026-09-21 11:55:11 -0300

    Create Continuar exercisio2.c

---
+++ Continuar exercisio2.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int idade[5];
+    int maiorIdade;
+    int menorIdade;
+    int mediaIdade
+    
+    for(contador=1; contador <= 5; contador ++){
+        printf("\n escreva sua idade : ");
+        scanf("%d", &idade[5]);
+      } 
+
+        return 0;
+  }
