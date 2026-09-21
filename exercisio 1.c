ac856ab5f4f8d5d9f217b23d77b497e99da0aaa9
Arthur Vargas Pinheiro <269752999+arthurpiro@users.noreply.github.com>
2026-09-21 11:36:06 -0300

    Update exercisio 1.c

--- exercisio 1.c
+++ exercisio 1.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+int main() {
+
+    float notas[4];
+    float contador;
+    float media;
+
+
+    for(contador=1; contador <= 4; contador ++){
+        printf("\n escreva a nota : ");
+        scanf("%f", &notas[4]);
+      } 
+
+     media= contador/4;
+         printf("%f""A sua media de notas é iqual a: ",media);
+  
+        
+    
+   
+    return 0;
+  }
+
