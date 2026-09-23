e93c0e0abbe3afd9e90c2bf0005d6377b6fc6570
Arthur Vargas Pinheiro <269752999+arthurpiro@users.noreply.github.com>
2026-09-23 08:47:51 -0300

    Update Continuar exercisio2.c

--- Continuar exercisio2.c
+++ Continuar exercisio2.c
@@ -1,20 +1,21 @@
-
-
 #include <stdio.h>
 
 int main() {
 
+    char nome;
     int idade[5];
     int maiorIdade;
     int menorIdade;
     int mediaIdade;
     int contador;
+    int soma;
 
     for(contador=1; contador <=5; contador++){
-  
+        printf("\n escreva seu nome: ");
+        scanf("%d", &nome);
         printf("\n escreva sua idade : ");
         scanf("%d", &idade[5]);
       } 
+    soma= soma+idade
 
         return 0;
-  }
